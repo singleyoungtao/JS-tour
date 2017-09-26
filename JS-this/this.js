e47b5709898fb304c845d1e9d523d0f4d13116bc
tao.liu <tao.liu@rea-group.com>
2017-09-26 16:03:25 +0800

    this - supplement

--- JS-this/this.js
+++ JS-this/this.js
@@ -8,6 +8,14 @@ function thisHandler1() {
 thisHandler1(); // test: hahaha this: window
 window.thisHandler1(); //
 
+function thisHandler11() {
+    var fruit = 'apple';
+    console.log(this.user); // undefined 全局上没有这个属性
+    console.log(this); // Window
+}
+thisHandler11();
+
+
 // 作为对象方法调用
 
 var thisHandler2 = {
@@ -49,13 +57,66 @@ testHandler();
 
 // 构造函数调用
 var name = 'apple';
-function Bar() {
+function fn1() {
     this.name = 'banana'
 }
 
-var handlerA = new Bar();
+var handlerA = new fn1();
 console.log(handlerA.name); // banaba
 console.log(name) // apple
+
+// 为什么this会指向handleA？
+// 首先new关键字会创建一个空的对象，
+// 然后会自动调用一个函数apply方法，
+// 将this指向这个空对象，这样的话函数内部的this就会被这个空的对象替代。
+
+
+function fn2() {
+    this.name = 'apple';
+}
+var handlerB = new fn2;
+console.log(handlerB.name); // apple
+
+function fn3() {
+    this.name = 'apple';
+    return {};
+}
+var handlerC = new fn3;
+console.log(handlerC.name); // undefined
+
+function fn4() {
+    this.name = 'apple';
+    return function(){};
+}
+var handlerD = new fn4;
+console.log(handlerD.name); // undefined
+
+function fn5() {
+    this.name = 'apple';
+    return 1;
+}
+var handlerE = new fn5;
+console.log(handlerE.name); // apple
+
+function fn6() {
+    this.name = 'apple';
+    return undefined;
+}
+var handlerF = new fn6;
+console.log(handlerF.name); // apple
+console.log(handlerF); // fn6 {name: "apple"}
+
+function fn7() {
+    this.name = 'apple';
+    return null;
+}
+var handlerG = new fn7;
+console.log(handlerG.name); // apple
+console.log(handlerG); // fn7 {name: "apple"}
+//还有一点就是虽然null也是对象，但是在这里this还是指向那个函数的实例，因为null比较特殊。
+//如果返回值是一个对象，那么this指向的就是那个返回的对象，
+//如果返回值不是一个对象那么this还是指向函数的实例。
+
 
 // apply call
 
@@ -87,3 +148,63 @@ thisHandler6.test(); // apple this指向window
 // 箭头函数中没有this绑定，需要查找作用域链决定
 // 如果箭头函数被非箭头函数包含， this绑定最近一层
 // 非箭头函数的this，否则，this的值会被设置为undefined
+
+// 闭包
+
+var name = 'apple';
+var thisHandler7 = {
+    name: 'banana',
+    test: () => {
+        return function () {
+            console.log('my name: ', this.name);
+        }
+    }
+}
+
+thisHandler7.test(); // 此时得到的是return的函数，其this指向的是window
+thisHandler7.test()(); // apple
+
+'use strict';
+var name = 'apple';
+var thisHandler71 = {
+    name: 'banana',
+    test: () => {
+        return function () {
+            console.log('my name: ', this.name);
+        }
+    }
+}
+
+thisHandler71.test(); // 此时得到的是return的函数，其this指向的是undefined
+thisHandler71.test()(); // 在严格版中的默认的this不再是window，而是undefined。
+
+var name = 'apple';
+var thisHandler8 = {
+    name: 'banana',
+    test: () => {
+        var that = this;
+        return function(){
+            console.log('my name: ', that.name);
+        }
+    }
+}
+
+thisHandler8.test();
+thisHandler8.test()(); // apple 由于箭头函数的缘故
+
+var name = 'apple';
+var thisHandler9 = {
+    name: 'banana',
+    test: function () {
+        var that = this;
+        return function() {
+            console.log('my name: ', that.name);
+        }
+    }
+}
+
+thisHandler9.test();
+thisHandler9.test()(); // banana 
+
+// 此处的that表示把this值赋给that，that本身并不是js关键字
+// 仅仅是一个变量赋值而已。
